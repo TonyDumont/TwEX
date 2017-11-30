57ceb7ff2a2e524c0b67a1faaa988a6974856eb6
tonyrdumont <tonyrdumont@gmail.com>
2017-11-30 01:06:23 -0500

    Added TEMPLATE to exchange

---
+++ TwEX_API/TwEX_API/Exchange/TEMPLATE.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System;
+
+namespace TwEX_API.Exchange
+{
+    class TEMPLATE
+    {
+        #region Properties
+        public static String thisClassName = "ExchangeClassName";
+        private static string ApiKey = String.Empty;
+        private static string ApiSecret = String.Empty;
+        private static RestClient client = new RestClient("API_URL");
+        #endregion Properties
+
+        #region API_Public
+        #region API_PUBLIC_Getters
+        // PUBLIC GETTERS
+        #endregion
+        #endregion API_Public
+
+        #region API_Private
+        #region API_PRIVATE_Getters
+        // ------------------------
+        // PRIVATE AUTH UTILS
+        // ------------------------
+        // PRIVATE GETTERS
+        #endregion
+        #region API_PRIVATE_Setters
+        // PRIVATE SETTERS
+        #endregion
+        #region API_PRIVATE_TBD
+        // TO BE DEVELOPED
+        #endregion
+        #endregion API_Private
+        
+        #region DataModels
+        #region DATAMODELS_Enums
+        // ENUMS
+        #endregion
+        #region DATAMODELS_Public 
+        // PUBLIC MODELS
+        #endregion
+        #region DATAMODELS_Private
+        // PRIVATE MODELS
+        #endregion
+        #endregion DataModels
+    }
+}
